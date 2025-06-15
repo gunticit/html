354f3a19e123edd69b1989e9c9df4cb868d35cb7
HungDoan <doananhlehung@gmail.com>
2025-06-16 06:40:41 +0700

    update html css

--- main.js
+++ main.js
@@ -7,3 +7,28 @@ $(document).on('click', function(e) {
         $('#mobileMenu').hide();
     }
 });
+
+$(document).on('click', '.accordion-header', function() {
+  var $item = $(this).closest('.accordion-item');
+  var $body = $item.find('.accordion-body');
+  var $arrow = $(this).find('.arrow');
+  if ($body.is(':visible')) {
+    $body.slideUp(180);
+    $(this).removeClass('active');
+  } else {
+    $item.siblings().find('.accordion-body').slideUp(180);
+    $item.siblings().find('.accordion-header').removeClass('active');
+    $body.slideDown(180);
+    $(this).addClass('active');
+  }
+});
+
+$(document).on('click', '#tab-batdau, #tab-tinhnang, #tab-khactphuc, #tab-baomat', function() {
+  $('#tab-batdau, #tab-tinhnang, #tab-khactphuc, #tab-baomat').removeClass('active');
+  $(this).addClass('active');
+  $('#content-batdau, #content-tinhnang, #content-khactphuc, #content-baomat').hide();
+  if (this.id === 'tab-batdau') $('#content-batdau').show();
+  if (this.id === 'tab-tinhnang') $('#content-tinhnang').show();
+  if (this.id === 'tab-khactphuc') $('#content-khactphuc').show();
+  if (this.id === 'tab-baomat') $('#content-baomat').show();
+});
